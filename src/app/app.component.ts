11f75f0633f357a98bbb108073700c928bf87853
José Eduardo <eduardolimacruz2@gmail.com>
2023-10-04 22:09:51 -0300

    git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { debounceTime, filter, map, of, switchMap } from 'rxjs';
+import { mergeWith, tap } from 'rxjs/operators';
+
+// tslint:disable-next-line:import-blacklist
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'typeahead-search';
+
+  myInput = new FormControl();
+  results = of([]).pipe(
+    mergeWith(this.myInput.valueChanges),
+    filter((v) => v.length > 2),
+    debounceTime(300),
+    map((v) => `https://swapi.dev/api/people/?search=${v}`),
+    tap((url) => console.log(url)),
+    switchMap((url) => this.http.get(url)),
+    map((json: any) => json['results'])
+  );
+
+  constructor(private http: HttpClient) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
